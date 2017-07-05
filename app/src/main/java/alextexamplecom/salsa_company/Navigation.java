da49a191f79f102a1a2e15859d056b911f358903
Alexander Treib <alextreib94@gmail.com>
2017-07-05 23:58:26 +0200

    Error in publishing data, but firebase integration works basically

--- app/src/main/java/alextexamplecom/salsa_company/Navigation.java
+++ app/src/main/java/alextexamplecom/salsa_company/Navigation.java
@@ -11,6 +11,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
 
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+//Firebase
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Navigation extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout m_drawerLayout;
@@ -18,6 +42,14 @@ public class Navigation extends AppCompatActivity implements NavigationView.OnNa
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.v_start);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.v_start);
